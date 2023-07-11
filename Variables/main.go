48641045ce4be38afd7c1f2bf6cb4d474188d79b
TanveerMM <tanveeratworks@gmail.com>
2023-07-11 12:05:24 +0530

    variables declarations

---
+++ Variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Rahul"
+	fmt.Println(name)
+
+	var num1, num2 int = 1, 2
+	fmt.Println(num1, num2)
+
+	var booleanValue = true
+	fmt.Println(booleanValue)
+
+	var number int
+	fmt.Println(number)
+
+	fruit := "Apple"
+	fmt.Println(fruit)
+}
